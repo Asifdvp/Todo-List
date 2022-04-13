41e02bebcbbe6f86faf8204674e08dbcf816d913
Asif Mustafayev <53805470+Asifdvp@users.noreply.github.com>
2022-04-14 01:49:27 +0400

    complete

--- app.js
+++ app.js
@@ -26,27 +26,40 @@ function filterTodo(e) {
   if (index === 0) {
     filter.src = "images/filterAZ.svg";
     // arr i A-Z siralamaq
+    
     arr.sort((a, b) => {
-      if (a > b) {
-        return 1;
-      } else if (a < b) {
-        return -1;
-      } else {
-        return 0;
-      }
+        if(isNaN(Number(a)) && isNaN(Number(b))){
+            if (a > b) {
+                return 1;
+              } else if (a < b) {
+                return -1;
+              } else {
+                return 0;
+              }
+        }
+        else{
+            return a-b;
+        }
+     
     });
     index++;
   } else {
     filter.src = "images/filterZA.svg";
     index--;
     arr.sort((a, b) => {
-      if (a > b) {
-        return -1;
-      } else if (a < b) {
-        return 1;
-      } else {
-        return 0;
-      }
+        if(isNaN(Number(a)) && isNaN(Number(b))){
+            if (a > b) {
+                return -1;
+              } else if (a < b) {
+                return 1;
+              } else {
+                return 0;
+              }
+        }
+        else{
+            return b-a;
+        }
+     
     });
   }
   form_div.innerHTML = "";
